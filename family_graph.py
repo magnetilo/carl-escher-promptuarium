5b5de8416159f1f9b868eb6b6fcd6555483828eb
Thilo Weber <thiloweber@Thilos-MacBook-Pro.local>
2024-09-07 12:33:40 +0200

    fix family_graph.py, add attribute husband_family_id

--- family_graph.py
+++ family_graph.py
@@ -22,6 +22,7 @@ class FamilyGraph:
             {
                 "family_id": None,
                 "father_family_id": "Billeter0001",
+                "husband_family_id": None,
                 "family_name": "Billeter",
                 "given_name": "Susanna",
                 "birth_year": 1615,
@@ -41,12 +42,13 @@ class FamilyGraph:
             raise Exception("person_attributes attributes missing. Required: family_id, father_family_id, family_name, given_name")
         
         # initialize other fields in person_attributes with None if not existing:
-        for key in ["birth_year", "death_year", "profession", "origin"]:
+        for key in ["husband_family_id", "birth_year", "death_year", "profession", "origin"]:
             if key not in person_attributes.keys():
                 person_attributes[key] = None
 
         # check if person is already in self.G:
         person_id = self.get_id_from_attributes(person_attributes)
+        # print(f"person_id: {person_id}")
 
         if person_id is not None:
             # Merge the new person_attributes with already existing ones ...
@@ -62,7 +64,7 @@ class FamilyGraph:
             self.G.add_node(person_id, **person_attributes)
             # Add person_id to self.family_id_index
             if person_attributes.get("family_id") is not None:
-                self.family_id_index["family_id"] = person_id
+                self.family_id_index[person_attributes["family_id"]] = person_id
         
         return person_id
     
@@ -92,7 +94,7 @@ class FamilyGraph:
             {
                 "family_id": None,
                 "father_family_id": "Billeter0001",
-                "surname": "Susanna",
+                "given_name": "Susanna",
                 "birth_year": 1615
             }
 
@@ -115,28 +117,32 @@ class FamilyGraph:
         #     # Person is already in self.G:
         #     return person_ids[0]
 
-        # Check if father with family_id is already in self.G:
-        if person_attributes['father_family_id'] is not None:
-            father_id = self.family_id_index.get(person_attributes['father_family_id'])
+        # Check if father or husband with family_id is already in self.G:
+        for pre_family_id in ["father_family_id", "husband_family_id"]:
+            if person_attributes[pre_family_id] is not None:
+                pre_node_id = self.family_id_index.get(person_attributes[pre_family_id])
+                # print(f"pre_family_id: {person_attributes[pre_family_id]}")
+                # print(f"pre_node_id: {pre_node_id}")
 
-            if father_id is not None:
-                # Father is already in self.G
-                # get all children:
-                children_ids = list(self.G.successors(father_ids[0]))
+                if pre_node_id is not None:
+                    # Father is already in self.G
+                    # get all children:
+                    children_ids = list(self.G.successors(pre_node_id))
 
-                # Filter children with same names only:
-                children_ids = [
-                    id for id in children_ids 
-                    if self.G.nodes[id]['surname'] == person_attributes['surname']
-                ]
-                if len(children_ids) > 1:
-                    # If multiple children with same name, filter for same birth_year only:
+                    # Filter children with same names only:
                     children_ids = [
                         id for id in children_ids 
-                        if self.G.nodes[id]['birth_year'] == person_attributes['birth_year']
+                        if self.G.nodes[id]['given_name'] == person_attributes['given_name']
                     ]
-                print(f"{person_attributes['surname']} is already in self.G!")
-                return children_ids[0]
+                    if len(children_ids) > 1:
+                        # If multiple children with same name, filter for same birth_year only:
+                        children_ids = [
+                            id for id in children_ids 
+                            if self.G.nodes[id]['birth_year'] == person_attributes['birth_year']
+                        ]
+                    if len(children_ids) > 0:
+                        # print(f"{person_attributes['given_name']} is already in self.G!")
+                        return children_ids[0]
 
         # Person doesn't exist yet in self.G:
         return None
@@ -147,7 +153,7 @@ class FamilyGraph:
         """
         plt.figure(figsize=(8, 6))
         pos = nx.spring_layout(self.G)  # Positions for all nodes
-        label1 = nx.get_node_attributes(self.G, 'surname')
+        label1 = nx.get_node_attributes(self.G, 'given_name')
         label2 = nx.get_node_attributes(self.G, 'family_name')
         labels = {
             key: label1.get(key, '') + ' ' + label2.get(key, '')
